ca312b803879112158f419cb9f7ff0fcf2596e82
Srdjan Obucina <obucinac@gmail.com>
2012-12-04 18:46:34 +0100

    Updated to download 1.0.1 release

--- get-ffmpeg-releases.sh
+++ get-ffmpeg-releases.sh
@@ -4,7 +4,7 @@ mkdir releases
 cd releases
 
 wget --content-disposition "http://git.videolan.org/?p=ffmpeg.git;a=snapshot;h=HEAD;sf=tgz"
-wget http://ffmpeg.org/releases/ffmpeg-1.0.tar.gz
+wget http://ffmpeg.org/releases/ffmpeg-1.0.1.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.11.2.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.10.6.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.9.2.tar.gz
