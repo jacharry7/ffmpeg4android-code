b93588c40aa7b52095d178d86c3dac1fe8461924
Срђан <obucina@gtv04-lin.domain.local>
2013-02-19 18:03:21 +0100

    Updated to download 1.1.2, 1.0.4 and 0.7.4 releases

--- get-ffmpeg-releases.sh
+++ get-ffmpeg-releases.sh
@@ -4,13 +4,13 @@ mkdir releases
 cd releases
 
 wget --content-disposition "http://git.videolan.org/?p=ffmpeg.git;a=snapshot;h=HEAD;sf=tgz"
-wget http://ffmpeg.org/releases/ffmpeg-1.1.tar.gz
-wget http://ffmpeg.org/releases/ffmpeg-1.0.1.tar.gz
+wget http://ffmpeg.org/releases/ffmpeg-1.1.2.tar.gz
+wget http://ffmpeg.org/releases/ffmpeg-1.0.4.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.11.2.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.10.6.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.9.2.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-0.8.12.tar.gz
-wget http://ffmpeg.org/releases/ffmpeg-0.7.13.tar.gz
+wget http://ffmpeg.org/releases/ffmpeg-0.7.14.tar.gz
 
 cd ..
 
